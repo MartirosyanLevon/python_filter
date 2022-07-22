ff1592c323f77938f86e34ecb74b7e4d8d885e3d
Levon Martirosyan <levon.martirosyan@dataart.com>
2022-07-22 13:02:06 +0400

    refactoring code

--- filter_manualy.py
+++ filter_manualy.py
@@ -3,12 +3,15 @@ def filter_manually(func, collection):
         if func(e):
             yield e
 
+
 if __name__ == '__main__':
     VOVELS = {'a', 'e', 'o', 'u', 'y', 'i'}
+
 
     def is_vowel(l):
         return l in VOVELS
 
+
     a = "this is a long string"
     for letter in filter(is_vowel, a):
-        print(letter,end=' ')
+        print(letter, end=' ')
